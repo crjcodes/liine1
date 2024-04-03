c13b6de734f019f71884e1cf04240d3aa0f11d4f
crjcodes <cindy.rae.tech@gmail.com>
2024-04-03 08:40:05 -0500

    launch profiles + beginnings of modules

--- src/api.py
+++ src/api.py
@@ -1,10 +1,17 @@
 from flask import Flask
-from operating_hours_manager import list_operating_restaurants
+from operating_hours_manager import *
 
 # TODO: parsing of the csv into the json data format on startup
 # ParsingManager -> Accessor
 
 app = Flask(__name__)
+
+@app.route('/restaurants/open')
+
+def handle_restaurants_open():
+
+  return list_operating_restaurants()
+
 
 @app.route("/")
 
@@ -14,10 +21,10 @@ def hello_world():
 
 # TODO: route to a HoursOfOperationManager.ListOpenRestaurants({datetime})
 #@app.route("/restaurant/open/<datetime>")
-@app.route('/a')
-def list_open_restaurants():
-  return "TODO"
+# @app.route('/a')
+# def list_open_restaurants():
+#   return "TODO"
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
 
