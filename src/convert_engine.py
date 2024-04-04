d0baf33ec64a5f19a451d34f98ab4bfc5b534fb8
crjcodes <cindy.rae.tech@gmail.com>
2024-04-04 12:36:01 -0500

    building out unit tests

--- src/convert_engine.py
+++ src/convert_engine.py
@@ -52,12 +52,12 @@ def MapLineToModel(line):
     raise ValueError()     
   
   restaurant_name = field_values[0]
-  opHours = ParseOpHours(field_values[1])
+  opHours = parse_op_hours(field_values[1])
 
   return
 
 
-def ParseOpHours(human_readable_op_hours):
+def parse_op_hours(human_readable_op_hours):
   """
   TODO: 
     Future: if performance concerns, compile the regex
@@ -69,27 +69,37 @@ def ParseOpHours(human_readable_op_hours):
   op_hours_list = []
 
   for section in sections:
-    section, close_time_text = ParseLastTimeText(section) 
-    section, open_time_text = ParseLastTimeText(section)
-    
-    dowList = ParseDow(section)
 
+    # Parse from input data
+
+    section, close_time_text = parse_last_time_text(section) 
+    section, open_time_text = parse_last_time_text(section)
+    
+    dow_list = parse_dow_text(section)
+
+    # Convert to non-primitive types    
     start_time = datetime.datetime.strptime(open_time_text, '%H:%M').time()
     end_time = datetime.datetime.strptime(close_time_text, '%H:%M').time()
 
-    # TODO: move to a constant list
-    for dow in "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun":
+    for dow in dow_list:
       op_hours = OperatingHours(dow, start_time, end_time)
       op_hours_list.append(op_hours)
 
   return op_hours_list
 
-def ParseLastTimeText(section):
-  
+def parse_last_time_text(section):
+
+  if (section is None or section == ""):
+    return section, None
+
   # non-overlapping
   # look for the last occurrence first
   # TODO: FUTURE: possible performance improvements with a different search, if needed
   match = re.search(r"(?s:.*)\s([0-9][0-9:]*\s*[ap]m)\s*", section)
+
+  if match == None:
+    return section, None
+
   timeText = match.group(1)
   section = section.replace(timeText, "")
   # TODO: as regex, in case spaces aren't there
@@ -99,7 +109,9 @@ def ParseLastTimeText(section):
   # TODO: error check on the match results
   return section, re.sub(r'\s', "", timeText)
 
-def ParseDowText(section):
+def parse_dow_text(section):
+    # TODO; right now, relying on exceptions for the error handling
+
     # TODO: move to common location
     dow_full = "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
     dow_list = []
@@ -109,21 +121,25 @@ def ParseDowText(section):
     for f in firstBreak:
       second_break = f.split("-")      
       start_dow = second_break[0].strip()
-      dow_list.append(start_dow)
       
-      if len(second_break) == 1:
+      if start_dow not in dow_full:
         continue
 
-      # TODO; right now, relying on exceptions for the error handling
-      df_start_index = dow_full.index(second_break[0])
-      df_end_index = dow_full.index(second_break[1])
+      df_start_index = dow_full.index(start_dow)
 
-      for f in dow_full[df_start_index:df_end_index]:
-        dow_list.append(f)             
+      if len(second_break) == 1:
+        dow_list.append(f.strip())
+      else:
+        end_dow = second_break[1].strip()
+
+        if end_dow not in dow_full:
+          continue
+
+        df_end_index = dow_full.index(end_dow)
+        for df in dow_full[df_start_index:df_end_index+1]:
+          dow_list.append(df)             
     
     return dow_list
-
-
 
 def findFirst(pattern, text):
   find = re.findall(pattern, text)
--- test/test_converter_engine.py
+++ test/test_converter_engine.py
@@ -5,46 +5,81 @@ import unittest
 import convert_engine
 
 class ConvertTests(unittest.TestCase):
-  def test_ParseLastTimeText_ShouldReturnTimeAsText_WhenGivenSampleData(self):
+
+#region parse_last_time_text
+  
+  # TODO: use input test data into one test, not two sets in sequence
+  def test_parse_last_time_text_ShouldReturnNone_WhenInvalidInput(self):
+    modifiedTestData, timeText = convert_engine.parse_last_time_text("")
+    self.assertIsNone(timeText)
+    self.assertEqual("", modifiedTestData)
+
+    modifiedTestData, timeText = convert_engine.parse_last_time_text("blah blah")
+    self.assertIsNone(timeText)
+    self.assertEqual("blah blah", modifiedTestData)
+
+  def test_parse_last_time_text_ShouldReturnTimeAsText_WhenGivenSampleData(self):
     testData = "Mon-Thu, Sun 11 am - 10 pm"
-    modifiedTestData, timeText = convert_engine.ParseLastTimeText(testData)
+    modifiedTestData, timeText = convert_engine.parse_last_time_text(testData)
     self.assertIsNotNone(timeText)
     self.assertEqual("10pm", timeText)
-
-  # def test_convert_from_returns_true(self):
-  #   result = convert_engine.ConvertFrom("test_input_data.csv")
-  #   self.assertTrue(result)
-  #   return
     
-  def test_ParseLastTimeText_ShouldReturnEndAndOpenTimeAsText_WhenGivenSampleData(self):
+  def test_parse_last_time_text_ShouldReturnEndAndOpenTimeAsText_WhenGivenSampleData(self):
     """
     More of a "sequence" test than a unit test
     """
 
     testData = "Mon-Thu, Sun 11 am - 10 pm"
 
-    modifiedTestData, timeText = convert_engine.ParseLastTimeText(testData)
+    modifiedTestData, timeText = convert_engine.parse_last_time_text(testData)
 
     self.assertIsNotNone(timeText)
     self.assertEqual("10pm", timeText)
     self.assertEqual("Mon-Thu, Sun 11 am", modifiedTestData)
 
-    modifiedTestData, timeText = convert_engine.ParseLastTimeText(modifiedTestData)
+    modifiedTestData, timeText = convert_engine.parse_last_time_text(modifiedTestData)
     self.assertIsNotNone(timeText)
     self.assertEqual("11am", timeText)
     self.assertEqual("Mon-Thu, Sun ", modifiedTestData)
+#endregion
 
-  def test_ParseDowText_ShouldReturnExpectedListofDaysOfWeek_WhenGivenSampleData(self):
-     testData = "Mon-Thu, Sun "
-     result = convert_engine.ParseDowText(testData)
+#region parse_dow_text
+  # TODO: break apart into test cases with parameterized test data
+  def test_parse_dow_text_ShouldReturnExpectedListofDaysOfWeek_WhenGivenBadData(self):
+     testData = "Mon-Thu, TBD "
+     result = convert_engine.parse_dow_text(testData)
 
-     self.assertIsNone(result)
+     self.assertIsNotNone(result)
      self.assertIsInstance(result, list)
-     self.assertEqual("Mon", list[0])
-     self.assertEqual("Tue", list[1])
-     self.assertEqual("Wed", list[2])
-     self.assertEqual("Thu", list[3])
-     self.assertEqual("Sun", list[4])
+     self.assertEqual("Mon", result[0])
+     self.assertEqual("Tue", result[1])
+     self.assertEqual("Wed", result[2])
+     self.assertEqual("Thu", result[3])
+
+     testData = "TBD-Thu, Sun"
+     result = convert_engine.parse_dow_text(testData)
+
+     self.assertEqual("Sun", result[0])
+
+     testData = "Mon-TBD, Sun"
+     result = convert_engine.parse_dow_text(testData)
+
+     self.assertEqual("Sun", result[0])
+
+  def test_parse_dow_text_ShouldReturnExpectedListofDaysOfWeek_WhenGivenSampleData(self):
+     testData = "Mon-Thu, Sun "
+     result = convert_engine.parse_dow_text(testData)
+
+     self.assertIsNotNone(result)
+     self.assertIsInstance(result, list)
+     self.assertEqual("Mon", result[0])
+     self.assertEqual("Tue", result[1])
+     self.assertEqual("Wed", result[2])
+     self.assertEqual("Thu", result[3])
+     self.assertEqual("Sun", result[4])
+#endregion
+
+
 
 
 if __name__ == '__main__':
